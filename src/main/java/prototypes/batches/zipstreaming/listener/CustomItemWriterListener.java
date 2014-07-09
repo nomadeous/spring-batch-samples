5563ed64a0b8e6baf575115c8e99a1a972ef02c9
nomadeous <jlamande@gmail.com>
2014-07-09 15:36:06 +0200

    Simpler

---
+++ src/main/java/prototypes/batches/zipstreaming/listener/CustomItemWriterListener.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prototypes.batches.zipstreaming.listener;
+
+import java.util.List;
+import org.springframework.batch.core.ItemWriteListener;
+
+public class CustomItemWriterListener implements ItemWriteListener<Object> {
+
+    @Override
+    public void beforeWrite(List<? extends Object> items) {
+        System.out.println("ItemWriteListener - beforeWrite");
+    }
+
+    @Override
+    public void afterWrite(List<? extends Object> items) {
+        System.out.println("ItemWriteListener - afterWrite");
+    }
+
+    @Override
+    public void onWriteError(Exception exception, List<? extends Object> items) {
+        System.out.println("ItemWriteListener - onWriteError");
+    }
+
+}
